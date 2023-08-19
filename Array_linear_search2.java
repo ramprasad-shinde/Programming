2157c376aaa345dc43cc95455243dba14625ab24
Ramprasad shinde <prasadshinde8788@gmail.com>
2023-08-19 20:06:42 +0530

    practice

---
+++ Array_linear_search2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Array_linear_search2 
+{
+    public static void votting(int age)
+    {
+        if(age >= 18 )
+        {
+            System.out.println("eliagble for vote");
+        }
+        else
+        {
+            System.out.println("not eliagble");
+        }
+    }
+   public static void main(String[] args) 
+   {
+         Scanner sc=new Scanner(System.in);
+         System.out.print("Enter a age :");
+         int age=sc.nextInt();
+         votting(age);
+   }    
+}
